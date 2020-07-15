1eede171815fea7b815fada8c58fe149049fc182
Lucas Pedroso <lucassppedroso@gmail.com>
2020-07-14 22:40:38 -0300

    criação das rotas PUT e DELETE

--- instructors.js
+++ instructors.js
@@ -86,3 +86,52 @@ exports.edit = function(req, res) {
     
     return res.render('instructors/edit', { instructor });
 }
+
+// PUT
+
+exports.put = function(req, res) {
+    const { id } = req.body;
+
+    let index = 0;
+
+    const foundInstructor = data.instructors.find(function(instructor, foundIndex){
+        if (id == instructor.id) {
+            index = foundIndex;
+            return true;
+        }
+    });
+
+    if(!foundInstructor) return res.send("Instructor not found!");
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor;
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 4), function(err) {
+        if(err) return res.send("Write file error!");
+
+        return res.redirect(`/instructors/${id}`);
+    });
+}
+
+// DELETE 
+
+exports.delete = function(req, res) {
+    const { id } = req.body;
+
+    const filteredInstructors = data.instructors.filter(function(instructor) {
+        return instructor.id != id;
+    });
+
+    data.instructors = filteredInstructors;
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 4), function(err) {
+        if(err) return res.send("Write file error!");
+
+        return res.redirect("/instructors");
+    });
+}
